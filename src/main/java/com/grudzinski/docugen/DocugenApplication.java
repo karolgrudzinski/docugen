d51d631e9e2972bb7433a40b9899c263f339ee04
Karol Grudziński <krlgrudzinski@gmail.com>
2019-01-14 00:01:03 +0100

    Initial commit. Just Spring Boot project from Initializr

---
+++ src/main/java/com/grudzinski/docugen/DocugenApplication.java
@@ -0,0 +1,14 @@
+package com.grudzinski.docugen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocugenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocugenApplication.class, args);
+    }
+
+}
+
