dad4585c49ac2b181c2d7cd03624d90f5a0ba0c3
Karol Grudziński <krlgrudzinski@gmail.com>
2019-02-14 17:44:33 +0100

    Created a view for previewing document that can be converted to PDF

--- src/main/java/com/grudzinski/docugen/controllers/WeddingCeremonyController.java
+++ src/main/java/com/grudzinski/docugen/controllers/WeddingCeremonyController.java
@@ -1,9 +1,12 @@
 package com.grudzinski.docugen.controllers;
 
+import com.grudzinski.docugen.model.document.WeddingCeremony;
 import com.grudzinski.docugen.services.WeddingCeremonyService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
@@ -26,4 +29,13 @@ public class WeddingCeremonyController {
 //        return TEMPLATE_PREFIX + "list";
         return "document/wedding/list";
     }
+
+    @RequestMapping({"/{id}/view"})
+    public String viewWedding(@PathVariable String id, Model model) {
+        WeddingCeremony weddingCeremony = weddingCeremonyService.findById(Long.valueOf(id));
+        model.addAttribute("wedding", weddingCeremony);
+
+        return "document/wedding/view";
+    }
+
 }
