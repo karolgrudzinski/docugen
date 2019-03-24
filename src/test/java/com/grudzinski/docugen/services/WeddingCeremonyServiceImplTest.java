15416721d2b5c6083564f48ee1a798439e561ea0
Karol Grudziński <krlgrudzinski@gmail.com>
2019-03-24 23:24:30 +0100

    Fix failing test for WeddingCeremonyServiceImpl

--- src/test/java/com/grudzinski/docugen/services/WeddingCeremonyServiceImplTest.java
+++ src/test/java/com/grudzinski/docugen/services/WeddingCeremonyServiceImplTest.java
@@ -67,11 +67,11 @@ public class WeddingCeremonyServiceImplTest {
         weddings.add(new WeddingCeremony());
         weddings.add(new WeddingCeremony());
 
-        when(weddingCeremonyRepository.findAll()).thenReturn(weddings);
+        when(weddingCeremonyRepository.findAll(any())).thenReturn(weddings);
 
         List<WeddingCeremony> weddingsReturned = weddingCeremonyService.getWeddings();
         assertEquals(3L, weddingsReturned.size());
-        verify(weddingCeremonyRepository).findAll();
+        verify(weddingCeremonyRepository).findAll(any());
         verify(weddingCeremonyRepository, never()).findById(anyLong());
     }
 
