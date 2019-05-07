3a3c8761434b7b4a0e09479a964a9078f0380b79
Karol Grudziński <krlgrudzinski@gmail.com>
2019-05-07 22:25:48 +0200

    Don't store password in a source code

--- src/main/java/com/grudzinski/docugen/WebSecurityConfig.java
+++ src/main/java/com/grudzinski/docugen/WebSecurityConfig.java
@@ -1,6 +1,8 @@
 package com.grudzinski.docugen;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,6 +12,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private Environment env;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -36,11 +42,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        
+
+        CharSequence key = env.getProperty("DOCUGEN_LOGIN_KEY");
+
         auth.inMemoryAuthentication()
                 .passwordEncoder(encoder)
                 .withUser("admin")
-                .password(encoder.encode("26iKyb0Ufa5w"))
+                .password((key != null) ? key.toString() : encoder.encode("admin"))
                 .roles("USER", "ADMIN");
     }
 }
